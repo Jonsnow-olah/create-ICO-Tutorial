bdc0c58d649cf157bde04202df99839aee76bff4
Olah Israel <olahisrael07@gmail.com>
2022-12-24 11:56:11 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xe0E33FE53d4e24b684bAa53e63B388E70b1E6f41";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS};
